7c8ca975272ac1370a293e929ba5908bf67f6acd
Aditya Simhadri <andy@ejamerica.com>
2020-04-27 18:05:24 -0700

    Added 2nd Test Class

---
+++ TestProject01/src/TestProjectGitWorld2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+/**
+ * @author andy
+ *
+ */
+import java.util.*;
+
+public class TestProjectGitWorld2 {
+
+	/**
+	 * 
+	 */
+	public TestProjectGitWorld2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner myObj1 = new Scanner(System.in);
+		
+		System.out.print("Enter username: ");
+		String strUsername = myObj1.nextLine();
+		
+		System.out.println("\r Username is: " + strUsername + "\n");
+		myObj1.close();
+	
+	}
+
+}
